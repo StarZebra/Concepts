7808a9788383a9cea73274b4e4be4032b1a35192
StarZebra <93840635+StarZebra@users.noreply.github.com>
2023-09-22 19:27:46 +0200

    improve fireveil methods

--- src/main/java/me/starzebra/concepts/listeners/FireVeilItem.java
+++ src/main/java/me/starzebra/concepts/listeners/FireVeilItem.java
@@ -10,14 +10,21 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.bukkit.scheduler.BukkitTask;
 
+import java.util.ArrayList;
+
 public class FireVeilItem implements Listener {
+
+    Plugin plugin = Concepts.getPlugin();
 
     boolean onCooldown = false;
 
     int mobsKilled = 0;
+    double circleRadius = 2.5;
+    int cylHeight = 6;
 
     @EventHandler
     public void onRightClick(PlayerInteractEvent event){
@@ -25,9 +32,6 @@ public class FireVeilItem implements Listener {
         if(!player.getInventory().getItemInMainHand().getType().equals(Material.BLAZE_POWDER)) return;
         if(event.getHand() == EquipmentSlot.OFF_HAND) return;
         if(event.getAction() == Action.LEFT_CLICK_AIR || event.getAction() == Action.LEFT_CLICK_BLOCK) return;
-
-
-
 
         if(onCooldown){
             player.sendMessage(Component.text("§cItem is currently on cooldown."));
@@ -41,8 +45,7 @@ public class FireVeilItem implements Listener {
             public void run() {
                 onCooldown = false;
             }
-        }.runTaskLaterAsynchronously(Concepts.getPlugin(), 100L);
-
+        }.runTaskLaterAsynchronously(plugin, 100L);
 
         World world = player.getWorld();
         world.playSound(player, Sound.ITEM_FLINTANDSTEEL_USE, 1, .2f);
@@ -51,28 +54,25 @@ public class FireVeilItem implements Listener {
             @Override
             public void run() {
                 Location loc = player.getLocation();
-                for (double i = 0; i < 7; i+=1.5){
-                    makeCircle(world, loc, 2.5, i);
-
-                    Location checkLoc = player.getLocation();
-                    for (Entity entity : checkLoc.getNearbyEntities(2.5, 2.5, 2.5)) {
-                        if(entity instanceof Player) continue;
-                        if(entity.getLocation().distance(checkLoc) >= 2.5) continue;
+                for (double i = 0; i <=cylHeight; i+=1.5){
+                    makeCircle(world, loc, circleRadius, i);
+                    getEntitiesInCyl(player, circleRadius).forEach( (entity -> {
                         entity.remove();
                         mobsKilled++;
-                    }
+                    }));
                 }
             }
-        }.runTaskTimer(Concepts.getPlugin(), 0, 3L);
+        }.runTaskTimer(plugin, 0, 2L);
 
         new BukkitRunnable() {
             @Override
             public void run() {
                 fireVeilTask.cancel();
+                if(mobsKilled <= 0) return;
                 player.sendMessage(Component.text("§eYour §6Fire Veil§e killed §6"+mobsKilled+ " §emobs."));
                 mobsKilled = 0;
             }
-        }.runTaskLater(Concepts.getPlugin(), 100L);
+        }.runTaskLater(plugin, 100L);
     }
 
     private void makeCircle(World world, Location location, double radius, double yOffset){
@@ -85,4 +85,29 @@ public class FireVeilItem implements Listener {
             world.spawnParticle(Particle.FLAME, pos, 0);
         }
     }
+
+    private ArrayList<Entity> getEntitiesInCyl(Player player, double radius){
+        ArrayList<Entity> stmt = new ArrayList<>();
+        Location playerLoc = player.getLocation();
+
+        Location maxCylCenter = playerLoc.clone().add(0,cylHeight,0);
+
+        for(Entity entity : player.getNearbyEntities(radius, radius, radius)){
+            //Check if entity is in cylinder
+            if(entity.getBoundingBox().getMinY() > maxCylCenter.getY()) continue;
+            if(entity.getBoundingBox().getMaxY() < playerLoc.getY()) continue;
+
+            double entityX = entity.getLocation().getX();
+            double entityZ = entity.getLocation().getZ();
+
+            double distance = Math.sqrt(Math.pow(entityX - playerLoc.getX(), 2) + Math.pow(entityZ - playerLoc.getZ(), 2));
+
+            if(distance <= radius){
+                stmt.add(entity);
+            }
+
+        }
+        return stmt;
+    }
+
 }
